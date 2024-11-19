dbdf66fe45f135b1e1ea837c4862f19ef9057469
MONK <169875752+DevNChill@users.noreply.github.com>
2024-11-20 01:15:50 +0530

    added class which will make todo

---
+++ src/js/makeTodo.js
@@ -0,0 +1,9 @@
+class MakeTodo {
+  constructor({ title, description, dueDate, priority }) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+export { MakeTodo };
