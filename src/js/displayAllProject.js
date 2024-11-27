a21e88d84ee4f90420baabd40d097175f6182aa0
MONK <169875752+DevNChill@users.noreply.github.com>
2024-11-27 17:25:31 +0530

    moved displayAllProject inside dom iife

--- src/js/displayAllProject.js
+++ src/js/displayAllProject.js
@@ -1,14 +1,15 @@
-import { Todo } from "./todo";
 import { project } from "./projects";
 import { createCustomElement } from "./helperfunction";
 
-const AllProjectBtn = document.getElementById("all-projects");
 const Main = document.querySelector("main");
-const dispalyAllProjects = () => {
-  const allProject = project.allProjects;
-  allProject.forEach((item, index) => {
-    const todoContainer = createCustomElement("div", item.pid, item.name);
-    Main.appendChild(todoContainer);
-  });
-};
-export { dispalyAllProjects };
+const dom = (function () {
+  const dispalyAllProjects = () => {
+    const allProject = project.allProjects;
+    allProject.forEach((item) => {
+      const todoContainer = createCustomElement("div", item.pid, item.name);
+      Main.appendChild(todoContainer);
+    });
+  };
+  return { dispalyAllProjects };
+})();
+export { dom};
--- src/js/helperfunction.js
+++ src/js/helperfunction.js
@@ -1,6 +1,7 @@
-function createCustomElement(tag, id, textcontent) {
+function createCustomElement(tag, id, textcontent, classlist = []) {
   let element = document.createElement(tag);
   element.id = id;
+  classlist.forEach((cls) => element.classList.add(cls));
   element.textContent = textcontent;
   return element;
 }
