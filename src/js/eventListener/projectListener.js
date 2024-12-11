ae3869032a5699074f1747ca03ea7fbb0f4fd936
MONK <169875752+DevNChill@users.noreply.github.com>
2024-12-11 09:29:17 +0530

    feat: user can now add project

--- src/js/eventListener/projectListener.js
+++ src/js/eventListener/projectListener.js
@@ -1,9 +1,13 @@
 import { dom } from "../dom";
+import { inputHandling } from "../form";
 import { logic } from "../logic";
 
 const listenersToProject = () => {
   const MAINHEADER = document.getElementById("__main_header");
   const PROJECTLIST = document.getElementById("__project_list");
+  const PROJECTFORM = document.getElementById("__project_form");
+  const CANCELPROJECT = document.getElementById("__project_cancel_btn_form");
+  const ADDPROJECT = document.getElementById("__add_project");
   PROJECTLIST.addEventListener("click", (e) => {
     console.log("hhhhhhhhh");
     if (
@@ -16,6 +20,21 @@ const listenersToProject = () => {
 
       dom.displayTodoOfClickedProject(pid);
     }
+  });
+
+  ADDPROJECT.addEventListener("click", (e) => {
+    inputHandling.openProjectDialog();
+  });
+  PROJECTFORM.addEventListener("submit", (e) => {
+    e.preventDefault();
+    logic.createNewProject(inputHandling.extractProjectInput());
+    dom.displayAllProjects();
+    inputHandling.closeProjectDialog();
+  });
+
+  CANCELPROJECT.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputHandling.closeProjectDialog();
   });
 };
 
