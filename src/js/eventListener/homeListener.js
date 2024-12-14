4863a47376d7b2c53e80c521115fba3d946d27a0
MONK <169875752+DevNChill@users.noreply.github.com>
2024-12-14 21:44:16 +0530

    removed bgcolor for clicked option

--- src/js/eventListener/homeListener.js
+++ src/js/eventListener/homeListener.js
@@ -5,25 +5,15 @@ const listenersToHome = () => {
 
   SECTION1.addEventListener("click", (e) => {
     const targetClass = e.target.closest(".section1-flex");
-    if (targetClass) {
-      const allSection1FlexElements =
-        SECTION1.querySelectorAll(".section1-flex");
-      allSection1FlexElements.forEach((item) =>
-        item.classList.remove("active"),
-      );
-      targetClass.classList.add("active");
-
-      if (targetClass.id === "__all_tasks") {
-        dom.displayAllTodos();
-      } else if (targetClass.id === "__today") {
-        dom.displayTodaysTodo();
-      } else if (targetClass.id === "__next_7_days") {
-        dom.displayWeeksTodo();
-      } else if (targetClass.id === "__important") {
-        dom.displayImportantTodos();
-      }
+    if (targetClass && targetClass.id === "__all_tasks") {
+      dom.displayAllTodos();
+    } else if (targetClass && targetClass.id === "__today") {
+      dom.displayTodaysTodo();
+    } else if (targetClass && targetClass.id === "__next_7_days") {
+      dom.displayWeeksTodo();
+    } else if (targetClass && targetClass.id === "__important") {
+      dom.displayImportantTodos();
     }
   });
 };
-
 export { listenersToHome };
